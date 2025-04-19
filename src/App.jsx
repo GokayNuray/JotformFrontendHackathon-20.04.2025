e1c08493399b4e4b4d1a81276e5d6accbf7e4dcc
GokayNuray <gokay.nuray@ug.bilkent.edu.tr>
2025-04-19 16:37:12 +0300

    Reactjs + TailwindCSS setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <p className="font-sans text-8xl bg-gradient-to-b bg-clip-text text-transparent from-blue-500 to-red-500">
+        Hello World!
+      </p>
+    </>
+  )
+}
+
+export default App
