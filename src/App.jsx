ddca7516bf77d1dafa462f3f0967a1c00391490f
GokayNuray <gokay.nuray@ug.bilkent.edu.tr>
2025-04-20 12:13:08 +0300

    App can now retrieve products in a desired format

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import {useEffect, useState} from "react";
+import {getFormInfo} from "./utils/jotformAPI.js";
+
 function App() {
+
+    const [formInfo, setFormInfo] = useState();
+    console.log(formInfo);
+
+    const products = formInfo?.["products"]?.map((product) => {
+        return {
+            pid: product.pid,
+            name: product.name,
+            description: product.description,
+            price: product.price,
+            images: JSON.parse(product.images),
+            quantity: 0,
+        }
+    });
+    console.log(products);
+
+    useEffect(() => {
+        if (!formInfo) {
+            setFormInfo("wait");
+            getFormInfo(setFormInfo);
+        }
+    }, []);
 
   return (
     <>
-      <p className="font-sans text-8xl bg-gradient-to-b bg-clip-text text-transparent from-blue-500 to-red-500">
-        Hello World!
-      </p>
+        {products ?
+            <p>{JSON.stringify(products)}</p>
+            :
+            <p>Loading</p>
+        }
     </>
   )
 }
